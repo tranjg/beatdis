263003d286ca9d3e1bc71a8c2d9459b5a9c42d38
justin <justintranthedeveloper@gmail.com>
2024-05-13 23:38:48 +0000

    SongInfo renders for multiple songs now

--- src/components/FileUpload.tsx
+++ src/components/FileUpload.tsx
@@ -12,6 +12,8 @@ import {
   DialogTitle,
 } from "@/components/ui/dialog.tsx";
 import SongInfo from "@/components/SongInfo";
+import axios from "axios";
+import formattedFilename from "@/utils/formattedFilename.ts";
 
 export default function FileUpload() {
   const [files, setFiles] = useState<Object>([]);
@@ -22,9 +24,8 @@ export default function FileUpload() {
 
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
+
     if (!files) return;
-    console.log(files);
-    // setFiles(undefined);
   };
 
   return (
@@ -40,13 +41,20 @@ export default function FileUpload() {
       {Object.values(files).length > 0 && (
         <div className="flex w-full items-center">
           <form onSubmit={handleSubmit} className="flex w-full flex-col gap-7">
-            <SongInfo />
+            {Object.values(files).map((arrayFiles) =>
+              arrayFiles.map((file: File, index: number) => {
+                const data = new FormData();
+                data.append("fileName", formattedFilename(file.name));
+                return <SongInfo key={index} data={data} />;
+              })
+            )}
+
             <DialogFooter>
               <button
                 type="submit"
-                className="bg-purple-600 border overflow-hidden rounded-md text-white font-bold cursor-pointer px-6 py-2 transition-all duration-200 ease-out hover:border-1 hover:border-purple-600  hover:bg-white hover:text-purple-600"
+                className="bg-primary border overflow-hidden rounded-md text-white font-bold cursor-pointer px-6 py-2 transition-all duration-200 ease-out hover:border-1 hover:border-primary hover:ring-1 ring-primary  hover:bg-white hover:text-primary"
               >
-                Save Changes
+                Save
               </button>
             </DialogFooter>
           </form>
